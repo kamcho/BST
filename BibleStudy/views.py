ec3f902ffbe047ca6322f800817d05773f9a87c1
kamcho <njokevin9@gmail.com>
2024-02-17 23:37:07 +0300

    pa

--- BibleStudy/views.py
+++ BibleStudy/views.py
@@ -348,30 +348,14 @@ def create_books_db():
     {'name': 'Jude', 'chapters': 1, 'order': 65},
     {'name': 'Revelation', 'chapters': 22, 'order': 66},
 ]
-# Database configuration
 
-        # books = Books.objects.all().order_by('order')
-        base_url = 'https://api.scripture.api.bible/v1/bibles'
-        endpoint = f'{base_url}/06125adad2d5898a-01/books'
-
-        headers = {
-            'api-key': '1cfeb0d5fb47d89b7bb6cef9e8427f6a',
-        }
-        # print(bible_id, book, chapter)
-        
-        try:
-            response = requests.get(endpoint, headers=headers)
-            response.raise_for_status()
-            data = response.json()
-            counter = 1
-            for book in data['data']:
-                bid = Books.objects.create(name=book['name'], abbreviation=book['abbreviation'],
-                                            book_id=book['id'], chapters=0, order=counter, location='OT')
-                counter = counter+1
-
-            return None
-        except:
-            pass
+        counter = 1
+        books = Books.objects.all().order_by('order')
+        for book in bible_books:
+            bid = books.get(order=book['order'])
+            bid.chapters = book['chapters']
+            bid.save()
+       
 
 
         # Assuming the API returns data in JSON format
