5618cf16379568014a430a8fe50dd7bf6a5e0d2d
kamcho <njokevin9@gmail.com>
2024-02-20 22:42:06 +0300

    pa

--- Payments/tests.py
+++ Payments/tests.py
@@ -1,12 +1,12 @@
+import base64
+import datetime
+import json
 import requests
 from requests.auth import HTTPBasicAuth
-from django.http import JsonResponse
 
-
-access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate'
-
-
+# Generate Mpesa Password
 def generate_access_token():
+    access_token_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
     
     consumer_key = "aSG8gGG7GWSGapToKz8ySyALUx9zIdbBr1CHldVhyOLjJsCz"
     consumer_secret = "o8qwdbzapgcvOd1lsBOkKGCL4JwMQyG9ZmKlKC7uaLIc4FsRJFbzfV10EAoL0P6u"
@@ -17,14 +17,63 @@ def generate_access_token():
 
     # return access_token from response
     if response.status_code == 200:
-        print(response,'\n\n')
-        access_token = response.json() 
-        print(access_token)
+        access_token = response.json()['access_token']
         
-        return "access_token"
+        return access_token
     
     else:
-        print('hey token',response.text,'\n\n')
-        return JsonResponse({'error': 'Token generation failed'}, status=response.status_code)
+        return None
     
-generate_access_token()
+
+def generate_mpesa_password(timestamp):
+    paybill = "174379" # This is the deafult test number
+    timestamp = timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    
+    # this is the default test passkey
+    pass_key = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919' 
+    concatenated_string = f"{paybill}{pass_key}{timestamp}"
+    base64_encoded = base64.b64encode(concatenated_string.encode()).decode('utf-8')
+    password = str(base64_encoded)
+
+    return password
+
+
+def initiate_stk_push():
+    # Define the STK push endpoint URL
+    stk_push_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    # Get the current timestamp in the required format
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    # Set the headers for the HTTP request
+    headers = {
+        'Authorization': f'Bearer {generate_access_token()}',
+        'Content-Type': 'application/json' }
+    # Prepare the payload for the STK push request
+    payload = {
+        "BusinessShortCode": 174379,
+        "Password": generate_mpesa_password(),
+        "Timestamp": timestamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": 1,
+        "PartyA": '254742134431',
+        "PartyB": 174379,
+        "PhoneNumber": '254742134431',
+        "CallBackURL": "https://mydomain.com/path",
+        "AccountReference": "Online.Store",
+        "TransactionDesc": "Payment TO Online Store" 
+    }
+    # Make the POST request to initiate the STK push
+    response = requests.post(stk_push_url, headers=headers, json=payload)
+    # Check the HTTP status code of the response
+    if response.status_code == 200:
+        # Parse the JSON response data
+        data = json.loads(response.text)
+        return data
+    else:
+        # Return an error message for non-200 status codes
+        return {'error': 'Failed to initiate STK push. Status Code:'}
+
+    
+    
+
+    
+
