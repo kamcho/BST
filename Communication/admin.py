2bf666e2ad1e29956cb1e287f302aee6aed521bb
kamcho <njokevin9@gmail.com>
2024-03-18 16:20:35 +0300

    fixed logout

--- Communication/admin.py
+++ Communication/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import MessagingSettings
+from .models import MessagingSettings, Inbox
 # Register your models here.
 admin.site.register(MessagingSettings)
+admin.site.register(Inbox)
